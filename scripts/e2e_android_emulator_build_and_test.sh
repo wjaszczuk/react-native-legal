7d48caa5dffb12d7fdc06148692959fce79d833c
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-06-06 12:06:35 +0200

    test: try record flow

--- scripts/e2e_android_emulator_build_and_test.sh
+++ scripts/e2e_android_emulator_build_and_test.sh
@@ -13,7 +13,7 @@ yarn workspace react-native-legal-bare-example e2e:android
 TEST_STATUS=$?
 echo "Test run completed with status $TEST_STATUS"
 adb shell "kill -2 \$(cat /data/local/tmp/screenrecord_pid.txt)"
-# Wait for the screen recording process to exit
+ Wait for the screen recording process to exit
 sleep 1
 adb pull /data/local/tmp/testRecording.mp4 .
 exit $TEST_STATUS
