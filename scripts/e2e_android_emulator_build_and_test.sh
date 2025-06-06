b9d430bac1c2e9f9f4ab3eb69996feb2a7fd6720
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-06-06 09:08:26 +0200

    test: try record flow

--- scripts/e2e_android_emulator_build_and_test.sh
+++ scripts/e2e_android_emulator_build_and_test.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+sdkmanager --list
+
 set -x
-yarn workspace react-native-legal-bare-example android:release
-adb shell "screenrecord --bugreport /data/local/tmp/recording.mp4 & echo \$! > /data/local/tmp/screenrecord_pid.txt" &
+echo "Starting the screen recording..."
+adb shell "screenrecord --bugreport /data/local/tmp/testRecording.mp4 & echo \$! > /data/local/tmp/screenrecord_pid.txt" &
 set +e
 yarn workspace react-native-legal-bare-example e2e:android
 TEST_STATUS=$?
+echo "Test run completed with status $TEST_STATUS"
 adb shell "kill -2 \$(cat /data/local/tmp/screenrecord_pid.txt)"
+# Wait for the screen recording process to exit
 sleep 1
-adb pull /data/local/tmp/recording.mp4 .
+adb pull /data/local/tmp/testRecording.mp4 .
 exit $TEST_STATUS
