0ba2d19a49487415b47c1b8293e4492b6ea31bd3
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-07-14 11:44:58 +0200

    chore: upgrade react native to 0.80.1 for bare-example

--- examples/bare-example/android/app/src/main/java/com/reactnativelegalbareexample/MainApplication.kt
+++ examples/bare-example/android/app/src/main/java/com/reactnativelegalbareexample/MainApplication.kt
@@ -4,13 +4,11 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
@@ -21,11 +19,11 @@ class MainApplication : Application(), ReactApplication {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
             }
- 
+
         override fun getJSMainModuleName(): String = "index"
- 
+
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
- 
+
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
@@ -35,10 +33,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
+    loadReactNative(this)
   }
 }
