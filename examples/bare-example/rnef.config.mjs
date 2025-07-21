d19a2c6643aa362957f3d9e1fa5eb7c263822b22
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-07-21 14:16:09 +0200

    chore: tryfix

--- examples/bare-example/rnef.config.mjs
+++ examples/bare-example/rnef.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { platformIOS } from '@rnef/platform-ios';
 import { platformAndroid } from '@rnef/platform-android';
+import { providerGitHub } from '@rnef/provider-github';
 import { pluginMetro } from '@rnef/plugin-metro';
+import 'dotenv/config';
 
 /** @type {import('@rnef/cli').Config} */
 export default {
@@ -10,5 +12,19 @@ export default {
     ios: platformIOS(),
     android: platformAndroid(),
   },
-  remoteCacheProvider: 'github-actions',
+  remoteCacheProvider: !hasGithubConfig()
+    ? null
+    : providerGitHub({
+        owner: process.env.GITHUB_REPOSITORY_OWNER,
+        repository: process.env.GITHUB_REPOSITORY_NAME,
+        token: process.env.GITHUB_TOKEN,
+      }),
 };
+
+function hasGithubConfig() {
+  return (
+    process.env.GITHUB_REPOSITORY_OWNER != null &&
+    process.env.GITHUB_REPOSITORY_NAME != null &&
+    process.env.GITHUB_TOKEN != null
+  );
+}
