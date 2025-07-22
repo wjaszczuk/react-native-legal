515f64f327c4cc97301105fe823d9e394cc2f96f
Wiktor Jaszczuk <wiktor.jaszczuk@callstack.com>
2025-07-22 08:44:41 +0200

    chore: prepare an example of ts type declaration export bug from RNEF

--- packages/react-native-legal/src/index.ts
+++ packages/react-native-legal/src/index.ts
@@ -1,1 +1,8 @@
+import type { PluginOutput } from '@rnef/config';
+
 export { ReactNativeLegal } from './ReactNativeLegal';
+
+export const pluginOutput: PluginOutput = {
+  name: 'some name',
+  description: 'This is a description',
+};
